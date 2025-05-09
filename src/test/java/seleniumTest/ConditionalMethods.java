852280b2291c5cb0dd99309c8c8337629df3e7eb
MirSiam10 <mir10siam@gmail.com>
2025-05-09 16:24:14 +0600

    push

--- src/test/java/seleniumTest/ConditionalMethods.java
+++ src/test/java/seleniumTest/ConditionalMethods.java
@@ -4,20 +4,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+            //
+//isDisplayed....isEnabled.....isSelected//
+            //
 public class ConditionalMethods {
 
     public static void main(String args[]){
         WebDriver driver = new ChromeDriver();
-        driver.get("https://demo.nopcommerce.com/");
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
 
         driver.manage().window().maximize();
 
         WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
         System.out.println(logo.isDisplayed());
 
-        boolean status = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
-        System.out.println("Logo status is : "+status);
+        //boolean status = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']")).isDisplayed();
+        //System.out.println("Logo status is : "+status);
+
+
+        boolean status2 = driver.findElement(By.xpath("//input[@id='FirstName']")).isEnabled();
+        System.out.println("First Name Field is selected : "+status2);
+
+        WebElement Male_Radio = driver.findElement(By.xpath("//input[@id='gender-male']"));
+        Male_Radio.click();
+        System.out.println("Male Radio button is: "+Male_Radio.isSelected());
+
+
+
+
+
 
     }
 
