fd42488d45f25d4b2a8e4253faf1463c3e411bbd
MirSiam10 <mir10siam@gmail.com>
2025-05-09 15:26:51 +0600

    first commit

---
+++ src/test/java/seleniumTest/Test1.java
@@ -0,0 +1,22 @@
+package seleniumTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("CSS Selector Testing methods");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://demo.nopcommerce.com/");
+
+        driver.manage().window().maximize();
+
+       // driver.findElement(By.cssSelector("#small-searchterms")).sendKeys("I-Phone");
+        //driver.findElement(By.linkText("Search")).click();
+
+        //driver.findElement(By.cssSelector("input.search-box-text")).sendKeys("I-Phone");
+        //driver.findElement(By.cssSelector("input[placeholder='Search store']")).sendKeys("iphone");
+        driver.findElement(By.cssSelector(".search-box-text[name='q']")).sendKeys("Iphone");
+    }
+}
---
+++ src/test/java/seleniumTest/Test2_XPATH.java
@@ -0,0 +1,23 @@
+package seleniumTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2_XPATH {
+
+    public static void main(String args[]){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demo.nopcommerce.com/");
+
+        //driver.findElement(By.xpath("//input[@placeholder='Search store']")).sendKeys("Ip");
+
+        //driver.findElement(By.xpath("//input[@placeholder='Search store' and @name = 'q']")).sendKeys("Ip");
+        driver.findElement(By.xpath("//a[text()='Apple MacBook Pro']")).click();
+
+    }
+
+
+}
